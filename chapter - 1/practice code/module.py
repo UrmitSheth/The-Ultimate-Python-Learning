8939c6179eb395579f49c22e79bb6f4da042bfb3
Sheth UrmitKumar Jagdishbhai <urmit.sheth212005@gmail.com>
2025-07-22 23:47:08 +0530

    Create module.py

---
+++ chapter - 1/practice code/module.py
@@ -0,0 +1,9 @@
+import pyjokes
+
+# print("print jokes...")   -->  use ctrl forward slass' / ' for fast doing comment(paund symbol)\  in vs code 
+'''
+  Its a multy line comment
+  ....
+'''
+joke = pyjokes.get_joke()
+print(joke)
