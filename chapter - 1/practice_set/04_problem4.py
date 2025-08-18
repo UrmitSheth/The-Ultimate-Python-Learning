6bb7ca983cee8b40b59c2e016e6dce75e813fb92
Sheth UrmitKumar Jagdishbhai <urmit.sheth212005@gmail.com>
2025-08-18 20:56:18 +0530

    Create 04_problem4.py

---
+++ chapter - 1/practice_set/04_problem4.py
@@ -0,0 +1,9 @@
+import os
+
+path = r"D:/My Works/Self-Coding/Python"
+contents = os.listdir(path)
+
+print(f"Contents of directory '{path}':")
+# for item in contents:
+#     print(item)
+print(contents)
