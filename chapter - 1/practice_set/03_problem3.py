b36538ef57fa481ebda650ce7464ebc4f47bd420
Sheth UrmitKumar Jagdishbhai <urmit.sheth212005@gmail.com>
2025-08-18 20:55:25 +0530

    Create 03_problem3.py

---
+++ chapter - 1/practice_set/03_problem3.py
@@ -0,0 +1,8 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("sarda ben no phone")
+engine.runAndWait()
+
+import emoji
+
+print(emoji.emojize(":grinning_face_with_big_eyes: :Loudly Crying: :snake: :fire:", language='alias'))
