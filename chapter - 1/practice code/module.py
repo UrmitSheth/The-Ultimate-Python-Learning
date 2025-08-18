acef0a7a609bacc994ea2d05adf20901cdf8fc57
Sheth UrmitKumar Jagdishbhai <urmit.sheth212005@gmail.com>
2025-08-18 20:52:44 +0530

    Delete chapter - 1/practice code directory

--- chapter - 1/practice code/module.py
+++
@@ -1,9 +0,0 @@
-import pyjokes
-
-# print("print jokes...")   -->  use ctrl forward slass' / ' for fast doing comment(paund symbol)\  in vs code 
-'''
-  Its a multy line comment
-  ....
-'''
-joke = pyjokes.get_joke()
-print(joke)
