142ade93e2007de28976478d26c6f327e1eef71e
Sheth UrmitKumar Jagdishbhai <urmit.sheth212005@gmail.com>
2025-08-22 10:17:46 +0530

    Create 02_problem2.py

---
+++ chapter - 4/practice_set/02_problem2.py
@@ -0,0 +1,22 @@
+Marks = []
+
+first = int(input("Enter the first Student Mark: "))
+Marks.append(first)
+
+second = int(input("Enter the second Student Mark: "))
+Marks.append(second)
+
+third = int(input("Enter the third Student Mark: "))
+Marks.append(third)
+
+four = int(input("Enter the fourth Student Mark: "))
+Marks.append(four)
+
+five = int(input("Enter the fifxth Student Mark: "))
+Marks.append(five)
+
+six = int(input("Enter the sixth Student Mark: "))
+Marks.append(six)
+
+Marks.sort()
+print("All Student Sorted Marks are: ",Marks)
