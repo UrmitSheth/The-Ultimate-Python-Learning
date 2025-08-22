653b015afaa5b8df8d5ceafd4aae6e2b00a45cf7
Sheth UrmitKumar Jagdishbhai <urmit.sheth212005@gmail.com>
2025-08-22 10:21:01 +0530

    Create 02_list_methods.py

---
+++ chapter - 4/02_list_methods.py
@@ -0,0 +1,16 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(friends)
+a = "a"
+friends.append("Mango")
+friends.append(a)
+print(friends)
+
+list1 = [66,100,6,34,90,56]
+# list1.sort()
+# list1.reverse()
+# list1.insert(2, 55) # Insert 55 such that its index in the list is 2
+# list1.remove(100)
+value = list1.pop(3)
+
+print(value)  
+print(list1)
