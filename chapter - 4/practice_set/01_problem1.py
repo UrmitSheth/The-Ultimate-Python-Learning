a68847150dbbce5554cb5bfa83640623cc4ff4a5
Sheth UrmitKumar Jagdishbhai <urmit.sheth212005@gmail.com>
2025-08-22 10:17:19 +0530

    Create 01_problem1.py

---
+++ chapter - 4/practice_set/01_problem1.py
@@ -0,0 +1,24 @@
+List = []
+
+first = input("Enter the first Fruit name: ")
+List.append(first)
+
+second = input("Enter the second Fruit name: ")
+List.append(second)
+
+third = input("Enter the third Fruit name: ")
+List.append(third)
+
+four = input("Enter the fourth Fruit name: ")
+List.append(four)
+
+five = input("Enter the fith Fruit name: ")
+List.append(five)
+
+six = input("Enter the sixth Fruit name: ")
+List.append(six)
+
+seven = input("Enter the seventh Fruit name: ")
+List.append(seven)
+
+print("The List of Fruits is: ",List)
