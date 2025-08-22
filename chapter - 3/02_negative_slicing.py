9a905b38701c335b5234841904e3b88b87be8a9d
Sheth UrmitKumar Jagdishbhai <urmit.sheth212005@gmail.com>
2025-08-22 10:13:28 +0530

    Create 02_negative_slicing.py

---
+++ chapter - 3/02_negative_slicing.py
@@ -0,0 +1,16 @@
+name = "Urmit_Sheth"
+
+print(name[0:6])
+
+print(name[-6:-1])
+
+print(name[1:7])
+
+
+print(name[:6]) # is same as print(name[0:6])
+print(name[1:]) # is same as print(name[1:11])
+print(name[1:11])
+
+# skip value 
+
+print(name[0:11:2])
