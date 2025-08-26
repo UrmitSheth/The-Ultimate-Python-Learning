67c2737e54f2226efdfdee0e04a3e86dcdb5c867
Sheth UrmitKumar Jagdishbhai <urmit.sheth212005@gmail.com>
2025-08-26 20:05:36 +0530

    Create 03_sets.py

---
+++ chapter - 5 /03_sets.py
@@ -0,0 +1,17 @@
+empty = set() # Creating an empty set
+# don't use s = {} as it creates an empty dictionary
+
+s = {1, 2, 3, 4, 5} # Creating a set with elements
+
+print(s) 
+
+# properties of sets
+# 1. Unordered: The elements in a set do not have a defined order.
+# 2. Unique: Sets do not allow duplicate elements.
+# 3. Mutable: Sets can be modified, meaning you can add or remove elements.
+# 4. Iterable: You can iterate through the elements of a set.
+# 5. No indexing: You cannot access elements in a set using an index like you do with lists or tuples.
+# 6. Dynamic size: Sets can grow or shrink as elements are added or removed.
+# 7. No slicing: You cannot slice a set like you do with lists or tuples.
+# 8. No nested sets: Sets cannot contain other sets as elements, but they can contain frozensets.
+# 9. Supports mathematical operations: Sets support operations like union, intersection, difference, and symmetric difference.
