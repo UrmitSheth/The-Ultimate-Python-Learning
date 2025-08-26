e054d8c4dd725f69be3ad1afc4c22e5cc893080e
Sheth UrmitKumar Jagdishbhai <urmit.sheth212005@gmail.com>
2025-08-26 20:04:34 +0530

    Create 01_dict.py

---
+++ chapter - 5 /01_dict.py
@@ -0,0 +1,22 @@
+details_of_Urmit = {
+    "name": "Urmit",
+    "age": 21,
+    "is_student": True,
+    "courses": ["Python", "JavaScript",  "Java"],
+    "address": {
+        "street": "A/402 Mangal Murti Hieghts",
+        "city": "Ahmedabad",
+        "state": "Gujarat",
+        "country": "India"
+    },
+    "marks": {
+        "Python": 90,
+        "JavaScript": 85,
+        "Java": 88
+     }
+    }
+
+print(details_of_Urmit)
+
+print(details_of_Urmit["name"])
+print(details_of_Urmit["marks"])
